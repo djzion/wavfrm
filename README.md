99ecf47c5ca9eb1b4fdc5279a412c3d27d70b456
Lincoln <bryant.lincoln@gmail.com>
2012-11-10 10:34:15 -0500

    Virtuenv + reqs

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ wavfrm
 ======
 
 Visual Waveform API
+
+Install
+==
+
+virtualenv env
+source env/bin/activate
+pip install -r requirements.txt
+
+You must have lame in /usr/bin/lame.  Use brew or get a binary
+brew install lame
+sudo ln -s /usr/local/bin/lame /usr/bin/lame
--- settings_common.py
+++ settings_common.py
@@ -88,7 +88,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'waveplayer.urls'
+ROOT_URLCONF = 'wavfrm.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
